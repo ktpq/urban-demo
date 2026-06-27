7d32eab0e1ad64a15710a2a58ceab599b6e88ed1
ktpq <kartpol47@gmail.com>
2026-06-27 16:41:54 +0700

    feat: set up scene component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import '@arcgis/map-components/components/arcgis-map';
+import "@arcgis/map-components/components/arcgis-scene";
 
 import { Component, signal, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -6,17 +7,7 @@ import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api-service';
 
 import { ArcgisMap } from '@arcgis/map-components/components/arcgis-map';
-
-import Polygon from '@arcgis/core/geometry/Polygon';
-
-import SimpleFillSymbol from '@arcgis/core/symbols/SimpleFillSymbol';
-import Graphic from '@arcgis/core/Graphic';
-
-// interface item{
-//   id: string;
-//   title: string;
-//   extent: any;
-// }
+import { ArcgisScene } from '@arcgis/map-components/components/arcgis-scene';
 
 @Component({
   selector: 'app-root',
@@ -26,10 +17,8 @@ import Graphic from '@arcgis/core/Graphic';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App implements OnInit {
-  // variables section
   mapComponent!: ArcgisMap;
-  // currentPolygon!: Polygon;
-  // urbanData!: any[];
+  sceneComponent!: ArcgisScene;
 
   constructor(
     private apiService: ApiService
@@ -40,50 +29,31 @@ export class App implements OnInit {
     console.log('Map is ready', event);
     this.mapComponent = event.target as ArcgisMap;
 
-    // if (this.currentPolygon){
-
-
-    //   const style = new SimpleFillSymbol({
-    //     color: "red",
-    //     outline: {
-    //       color: "blue",
-    //       width: 2
-    //     }
-    //   })
-
-    //   const graphic = new Graphic({
-    //     symbol: style,
-    //     geometry: this.currentPolygon
-    //   });
-
-    //   this.mapComponent.view.graphics.add(graphic);
-    // }
 
     this.mapComponent.view.on("click", (event) => {
       console.log(event.mapPoint);
     })
   }
 
-  ngOnInit() {
-    this.apiService.getUrbanProjects().subscribe({
-      next: (response) => {
-        // this.urbanData = response.data.urbanModels;
-        // const rings =  response.data.urbanDesignDatabase.projects[0].geometry.rings;
-        console.log("response", response)
-        // const polygon = new Polygon({
-        //   rings: rings,
-        //   spatialReference: {wkid: 3857}
-        // })
+  onSceneReady(event: CustomEvent) {
+    console.log('Scene is ready', event);
+    this.sceneComponent = event.target as ArcgisScene;
 
-        // this.currentPolygon = polygon as Polygon;
-
-        // console.log(this.currentPolygon);
-        
-      },
-      error: (error) => {
-        console.log(error);
-      }
+    this.sceneComponent.view.on("click", (event) => {
+      console.log(event);
     })
+  }
+
+  ngOnInit() {
+    // this.apiService.getUrbanProjects().subscribe({
+    //   next: (response) => {
+    //     console.log("response", response)
+        
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //   }
+    // })
     
   }
 }
