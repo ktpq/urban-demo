3431f6e090bc1c07bb94f95e41cc5b28b269df94
ktpq <kartpol47@gmail.com>
2026-07-02 13:46:53 +0700

    fix: update urbanApiKey in environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    urbanApiKey: "aERGfeDHPDW1wUYQDmaOrPg..MiXNEcwPn00cfxFoO658Y9ryxGORwxF-1Phxzyn1Y-RM5k8fumQKpBPIjTO2TmOMTKMqgUkClYIOxU4Xbi0St-Ih9P4NXTdu5_mlQo3Gjmx-HDGApNw9l-KlhL08x8B9Gi2eikc8IBkqVJE.",
+    urbanApiKey: "axmVlo89SQBM8-g3ot-xUOw..D0ClfrZf4hHIT7xGrOG_nQ5_bMsBLPIEox8bWi-p5whrezeIyOd_Zov9acggDouE2aQkyy7nW0qywumajxCo94qLH8gILz2UULK26RLeHdmO5wfQchcVpxOjQGnwkK1sASI0royCU5ZjW1g.",
     urbanApiUrl: "https://urban-api.arcgis.com/graphql",
 };
